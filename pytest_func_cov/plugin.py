60c64e639dbcb7c3064f3342af71e25298adb366
Radu Ghitescu <radu.ghitescu@gmail.com>
2020-02-15 10:51:29 +0000

    Initial commit

---
+++ pytest_func_cov/plugin.py
@@ -0,0 +1,231 @@
+from functools import wraps
+import importlib.util
+import inspect
+import os
+import sys
+
+
+def import_module_from_file(file_path, package_name=None, module_name=None):
+    """
+    Imports module from a given file path. If no package name is given, it is
+    treated as a base package.
+
+    Args:
+        file_path (str): Path to module, assumed to be a ".py" file
+        package_name (str): Name of the parent package, defaults to None
+        module_name (str): Name of the module, defaults to None. If not set,
+            the name of the file excluding the .py extension is used
+
+    Returns:
+        Module object
+    """
+    if module_name is None:
+        module_name = os.path.basename(file_path).rstrip(".py")
+
+    if package_name is not None:
+        module_name = f"{package_name}.{module_name}"
+
+    spec = importlib.util.spec_from_file_location(module_name, file_path)
+    module = importlib.util.module_from_spec(spec)
+    sys.modules[module_name] = module
+    spec.loader.exec_module(module)
+
+    return module
+
+
+def is_defined_in_module(o, module):
+    """
+    Checks if an object is defined in a given module and not imported.
+
+    Args:
+        o (object): Object to check
+        module (module): Module
+
+    Returns:
+        True or False
+    """
+    return o.__module__ == module.__name__
+
+
+def get_functions_defined_in_module(module):
+    """
+    Get all the functions defined in a given module.
+
+    Args:
+        module (module): Module for lookup
+
+    Returns:
+        List(tuple(str, o))
+    """
+    all_functions = inspect.getmembers(module, inspect.isfunction)
+    functions_defined_in_module = [
+        f for f in all_functions if is_defined_in_module(f[1], module)
+    ]
+
+    return functions_defined_in_module
+
+
+def get_classes_defined_in_module(module):
+    """
+    Get all the classes defined in a given module.
+
+    Args:
+        module (module): Module for lookup
+
+    Returns:
+        List(tuple(str, o))
+    """
+    all_classes = inspect.getmembers(module, inspect.isclass)
+    classes_defined_in_module = [
+        f for f in all_classes if is_defined_in_module(f[1], module)
+    ]
+
+    return classes_defined_in_module
+
+
+def find_packages(path):
+    """
+    Finds all packages in a given directory. A directory is a package if
+    it contains a file named __init__.py.
+
+    Args:
+        path (str): Base lookup directory
+
+    Returns:
+        List(str) absolute paths to the package
+    """
+    directories_in_path = [
+        os.path.join(path, d) for d in os.listdir(path) if os.path.isdir(d)
+    ]
+
+    packages = [d for d in directories_in_path if "__init__.py" in os.listdir(d)]
+
+    return packages
+
+
+def find_modules(path):
+    """
+    Finds all modules in a given directory. A file is a module if it has a
+    .py extension and is not named __init__.py.
+
+    Args:
+        path (str): Base lookup directory
+
+    Returns:
+        List(str) absolute paths to the modules
+    """
+    modules_in_path = [
+        os.path.join(path, m) for m in os.listdir(path) if m.endswith(".py") and m != "__init__.py"
+    ]
+
+    return modules_in_path
+
+
+class FunctionCallMonitor:
+    def __init__(self):
+        self._functions = {}
+        self._target_modules = []
+
+    def register_function(self, f):
+        self._functions[f] = 0
+
+        @wraps(f)
+        def _(*args, **kwargs):
+            called_from = inspect.stack()[1].filename
+            self.record_call(f, called_from)
+
+            return f(*args, **kwargs)
+
+        return _
+
+    @property
+    def registered_functions_count(self):
+        return len(self._functions)
+
+    @property
+    def called_functions_count(self):
+        return len([f for f in self._functions.values() if f > 0])
+
+    def register_target_module(self, m):
+        self._target_modules.append(m)
+
+    def record_call(self, f, m):
+        funcs = self._functions
+        if m in self._target_modules:
+            funcs[f] = funcs[f] + 1
+
+
+function_call_monitor = FunctionCallMonitor()
+
+
+def index_module(module):
+    """
+    Decorates all functions and classes defined in the given module
+    for function call monitoring.
+
+    Args:
+        module (module): Module to index
+    """
+    functions = get_functions_defined_in_module(module)
+    classes = get_classes_defined_in_module(module)
+
+    for f_name, f in functions:
+        setattr(module, f_name, function_call_monitor.register_function(f))
+
+
+def register_package(package_path):
+    """
+    Imports package __init__ and all child modules and packages recursively
+    and registers each module for function call monitoring.
+
+    Args:
+        package_path (str): Path to package
+    """
+    package_name = os.path.basename(package_path)
+    package_import_path = os.path.join(package_path, "__init__.py")
+
+    package = import_module_from_file(package_import_path, module_name=package_name)
+
+    index_module(package)
+
+    for module_path in find_modules(package_path):
+        module = import_module_from_file(module_path, package_name=package_name)
+        index_module(module)
+
+    for child_package in find_packages(package_path):
+        register_package(child_package)
+
+
+def discover(root_path):
+    """
+    Finds, registers and indexes all packages in a given folder.
+
+    Args:
+        root_path (str): Lookup path
+    """
+    packages = find_packages(root_path)
+
+    # Filter for commonly known test folders
+    packages = [p for p in packages if os.path.basename(p) not in ["tests", "test"]]
+
+    # Register packages
+    for package in packages:
+        register_package(package)
+
+
+def pytest_sessionstart(session):
+    discover(session.fspath)
+
+
+def pytest_collect_file(path, parent):
+    function_call_monitor.register_target_module(path)
+
+
+def pytest_terminal_summary(terminalreporter, exitstatus, config):
+    functions_found = function_call_monitor.registered_functions_count
+    functions_called = function_call_monitor.called_functions_count
+    coverage = functions_called / functions_found
+
+    terminalreporter.write(f"Found {functions_found} functions and methods.\n")
+    terminalreporter.write(f"Called {functions_called} functions and methods.\n")
+    terminalreporter.write(f"Function call coverage: {round(coverage * 100, 2)}%\n")
