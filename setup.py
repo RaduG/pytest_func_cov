588bf3af3d62ddbffb1243677c2985ce67d4e543
Radu Ghitescu <radu.ghitescu@gmail.com>
2020-02-17 16:09:58 +0000

    Bump to v0.1.1

--- setup.py
+++ setup.py
@@ -3,13 +3,13 @@ from setuptools import setup
 setup(
     name="pytest_func_cov",
     packages=["pytest_func_cov"],
-    version="0.1",
+    version="0.1.1",
     license="MIT",
     description="Pytest plugin for measuring function coverage",
     author="Radu Ghitescu",
     author_email="radu.ghitescu@gmail.com",
     url="https://github.com/radug0314/pytest_func_cov",
-    install_requires=["pytest>=5.3"],
+    install_requires=["pytest>=5"],
     entry_points={"pytest11": ["pytest_func_cov = pytest_func_cov.plugin"]},
     classifiers=[
         "Framework :: Pytest",
