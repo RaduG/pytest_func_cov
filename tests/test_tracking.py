c77ace41f8c4c045a60461d5eb1452e56a8e5d93
vladg94 <vladg94@users.noreply.github.com>
2020-04-05 07:55:35 -0700

    Add Tests For find_package function (#5)
    * Add Tests For find_package function
    
    * Add scope to pytest.fixture packag/non_package
    
    * Mistake removal#
    
    * Modified test for find_package with packages
    
    * remove print statements
    
    * Add tests for get_functions defined in module.
    Reformated file with black.
    
    * Fix tests
    
    Co-authored-by: Radu Ghitescu <radu.ghitescu@gmail.com>

--- tests/test_tracking.py
+++ tests/test_tracking.py
@@ -6,6 +6,7 @@ from pytest_func_cov import tracking
 
 from .test_package.classes import SimpleClass
 from .test_package.functions import simple_function, lambda_
+from .test_package import functions
 
 
 @pytest.mark.parametrize(
@@ -32,10 +33,25 @@ def package_path():
             yield folder
 
 
-@pytest.fixture
+@pytest.fixture(scope="module")
 def non_package_path():
     with tempfile.TemporaryDirectory() as folder:
         yield folder
+
+
+@pytest.fixture
+def directory_with_packages():
+    """
+    Returns
+        tuple(str, list(str)): First element is the absolute path to the root folder,
+            the second element is the list of expected package names
+    """
+    with tempfile.TemporaryDirectory() as root_folder:
+        with tempfile.TemporaryDirectory(
+            prefix=f"{os.path.basename(root_folder)}/"
+        ) as package_folder:
+            with open(os.path.join(package_folder, "__init__.py"), "w"):
+                yield root_folder, [package_folder]
 
 
 def test_is_package_with_package(package_path):
@@ -54,5 +70,64 @@ def test_get_methods_defined_in_class():
         SimpleClass.simple_method,
         SimpleClass.simple_static_method,
     ]
-    print(output, expected)
-    assert all([method in output for method in expected])
+    assert len(output) == len(expected) and all(
+        [method in output for method in expected]
+    )
+
+
+def test_find_packages_in_directory_without_package(non_package_path):
+    packages = tracking.find_packages(non_package_path)
+
+    assert packages == []
+
+
+def test_find_packages_in_directory_with_package(directory_with_packages):
+    root_directory, expected = directory_with_packages
+
+    output = tracking.find_packages(root_directory)
+
+    assert len(output) == len(expected) and all(
+        [package in output for package in expected]
+    )
+
+
+@pytest.fixture
+def directory_with_modules():
+    """
+    Returns
+        tuple(str, list(str)): First element is the absolute path to the root folder,
+            the second element is the list of expected module names
+    """
+    with tempfile.TemporaryDirectory() as folder:
+        with tempfile.NamedTemporaryFile(
+            prefix=f"{os.path.basename(folder)}/", suffix=".py"
+        ) as f:
+            yield folder, [f.name]
+
+
+def test_find_modules_in_directory_without_modules(non_package_path):
+    output = tracking.find_packages(non_package_path)
+    assert output == []
+
+
+def test_find_module_in_directory_with_modules(directory_with_modules):
+    directory, expected = directory_with_modules
+    output = tracking.find_modules(directory)
+
+    assert len(output) == len(expected) and all(
+        [module in output for module in expected]
+    )
+
+
+def test_get_functions_defined_in_module():
+    output = tracking.get_functions_defined_in_module(functions)
+    expected = [
+        ("simple_function", functions.simple_function),
+        ("lambda_", functions.lambda_),
+    ]
+    print(output, expected, flush=True)
+    assert all([function in output for function in expected])
+
+
+def test_is_defined_in_module():
+    pass
